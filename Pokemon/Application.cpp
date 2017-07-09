c596be519ec37ba6aff80886d86e8bd736e4ae11
GM2000 <GM2000@outlook.com>
2017-07-09 16:35:24 +0800

    Update to SDL2

--- Pokemon/Application.cpp
+++ Pokemon/Application.cpp
@@ -5,29 +5,22 @@
 
 void Application::run()
 {
-	SDL_Surface* screen = NULL;
-	SDL_Surface* hello = NULL;
-
-	SDL_Surface* icon = IMG_Load("Resources\\ICON.ico");
-
 	SDL_Init(SDL_INIT_EVERYTHING);
 
-	screen = SDL_SetVideoMode(512, 512, 32, SDL_SWSURFACE);
-	SDL_WM_SetCaption(m_title, m_title);
-	SDL_WM_SetIcon(icon, nullptr);
+	SDL_Window* sdlWindow;
+	SDL_Renderer* render;
 
-	hello = IMG_Load("Resources\\test.jpg");
-	SDL_BlitSurface(hello, NULL, screen, NULL);
+	SDL_CreateWindowAndRenderer(512, 512, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN, &sdlWindow, &render);
+	SDL_Texture* texture = IMG_LoadTexture(render, "Resources\\test.jpg");
 
-	SDL_Flip(screen);
+	SDL_RenderClear(render);
+	SDL_RenderCopy(render, texture, nullptr, nullptr);
+	SDL_RenderPresent(render);
+
 	SDL_Event sdlEvent = SDL_Event();
 
 	while (sdlEvent.type != SDL_QUIT)
 		while (SDL_PollEvent(&sdlEvent));
-
-	//SDL_Delay(2000);
-
-	SDL_FreeSurface(hello);
 
 	SDL_Quit();
 }
